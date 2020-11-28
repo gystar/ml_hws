03b601993a26d73f594693a9eb5485f61962d592
gystar <gy163email@163.com>
2020-11-28 20:50:05 +0800

    使用alextnet进行分类

--- hw3/main.py
+++ hw3/main.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
 import pandas as pd
-import os,sys
+import os
+import sys
 import multiprocessing
 import importlib
 import image_set  # 图片数据集实现类
@@ -9,7 +10,7 @@ import model_manager
 import image_classification  # 分类模型实现类
 
 
-#处理思路：数据处理（引用函数dataset）->模型实例化、损失函数、优化器->
+# 处理思路：数据处理（引用函数dataset）->模型实例化、损失函数、优化器->
 # dataloader构造->循环训练模型（寻找参数）
 
 # 如果自定义的模块代码改变，需要reload
@@ -18,15 +19,16 @@ importlib.reload(model_manager)
 importlib.reload(image_classification)
 
 current_dir = os.path.dirname(__file__)
-TRAIN_DIR = os.path.join(current_dir ,"data/training")
-VALIDATION_DIR = os.path.join(current_dir ,"data/validation")
-TEST_DIR = os.path.join(current_dir ,"data/testing")
-SAVE_PATH = os.path.join(current_dir ,"model.pkl")
-TEST_REULST_PATH = os.path.join(current_dir ,"./data/result.csv")
+TRAIN_DIR = os.path.join(current_dir, "data/training")
+VALIDATION_DIR = os.path.join(current_dir, "data/validation")
+TEST_DIR = os.path.join(current_dir, "data/testing")
+SAVE_PATH = os.path.join(current_dir, "model.pkl")
+TEST_REULST_PATH = os.path.join(current_dir, "./data/result.csv")
 
 # 用以对cpu和GPU进行兼容
 cuda_ok = torch.cuda.is_available()
 print("Cuda is available" if cuda_ok else "There is no gpu available.")
+
 
 def CalcRightCount(y, t):
     # 计算预测正确的数量，y、t均为numpy一维数组
@@ -36,71 +38,76 @@ def CalcRightCount(y, t):
             count += 1
     return count
 
+
 def validate(data_loader, m):
-    m.eval()  # 会关闭dropout、batchnorm等  
-    right_count = 0    
+    m.eval()  # 会关闭dropout、batchnorm等
+    right_count = 0
     with torch.no_grad():  # 不构建计算图
         for _, info in enumerate(data_loader):
             images, labels = info
             if cuda_ok:
                 images, labels = images.cuda(), labels.cuda()
-            y_pred = m(images).squeeze()            
+            y_pred = m(images).squeeze()
             right_count += CalcRightCount(
                 np.argmax(y_pred.cpu().numpy(), 1), labels.cpu().numpy())
             del images, labels, y_pred
-            torch.cuda.empty_cache()            
+            torch.cuda.empty_cache()
     return right_count
+
 
 # 训练数据集
 data_train = image_set.LearningSet(
     TRAIN_DIR, image_classification.GYHF_AlexNet.input_size)
-class_count = data_train.GetClassNum()  # 获取类别数量 
-    
-#模型实例化
+class_count = data_train.GetClassNum()  # 获取类别数量
+
+# 模型实例化
 if os.path.exists(SAVE_PATH):
     print("model has been loaded from file.")
     model = torch.load(SAVE_PATH)
 else:
     print("create a new model.")
     model = image_classification.GYHF_AlexNet(class_count)
-    
+
 if cuda_ok:
     try:  # 如果显存不够，可能无法用GPU进行计算
         model = model.cuda()
     except:
         print("There is no enough GPU memory for model,use cpu instead.")
-        model=model.cpu()
+        model = model.cpu()
         cuda_ok = False
 
 nbatch_predict = 1024
-#验证集数据加载器
-#训练数据验证
+# 验证集数据加载器
+# 训练数据验证
 data_validation1 = image_set.LearningSet(
-                TRAIN_DIR, model.input_size, False)
+    TRAIN_DIR, model.input_size, False)
 data_loader_validation1 = torch.utils.data.DataLoader(
-                data_validation1, nbatch_predict, shuffle=False,
-                num_workers=multiprocessing.cpu_count())
-#验证集数据验证
+    data_validation1, nbatch_predict, shuffle=False,
+    num_workers=multiprocessing.cpu_count())
+# 验证集数据验证
 data_validation2 = image_set.LearningSet(
-                VALIDATION_DIR, model.input_size, False)
+    VALIDATION_DIR, model.input_size, False)
 data_loader_validation2 = torch.utils.data.DataLoader(
-                data_validation2, nbatch_predict, shuffle=False,
-                num_workers=multiprocessing.cpu_count())
-validate(data_loader_validation1,model)
-#训练模型
+    data_validation2, nbatch_predict, shuffle=False,
+    num_workers=multiprocessing.cpu_count())
+
+# 训练模型
 print("waiting for training...")
-for i in range(1):
-    model = model_manager.train_model(model, data_train,cuda_ok = cuda_ok, epochs = 10)
+for i in range(20):
+    print("iters ", i, " ...")
+    model = model_manager.train_model(
+        model, data_train, cuda_ok=cuda_ok, epochs=10)
     with torch.no_grad():
-        #每10轮保存一次模型，同时验证一下正确率
+        # 每10轮保存一次模型，同时验证一下正确率
         # 模型保存
         torch.save(model, SAVE_PATH)
         # 用验证集和训练集验证:
         print("waiting for validation...")
-        train_accuracy = 100*validate(data_loader_validation1,model)/data_validation1.GetLen()
-        print("train accuracy:", train_accuracy, "%")            
+        train_accuracy = 100 * \
+            validate(data_loader_validation1, model)/data_validation1.GetLen()
+        print("train accuracy:", train_accuracy, "%")
         validation_accuracy = 100 * \
-        validate(data_loader_validation2,model)/data_validation2.GetLen()
+            validate(data_loader_validation2, model)/data_validation2.GetLen()
         print("validation accuracy:", validation_accuracy, "%")
 
 # 测试集
--- hw3/model_manager.py
+++ hw3/model_manager.py
@@ -13,50 +13,51 @@ import math
 importlib.reload(image_set)
 importlib.reload(image_classification)
 
-def train_model(model , data, nbatch = 128, epochs = 10, cuda_ok = True):             
-    #dataloader   
+
+def train_model(model, data, nbatch=128, epochs=10, cuda_ok=True):
+    # dataloader
     data_loader_train = torch.utils.data.DataLoader(
         data,
         batch_size=nbatch,
         num_workers=math.floor(multiprocessing.cpu_count()),  # 线程数等于cpu核数
         pin_memory=False,
         shuffle=True)
-    
+
     # 损失函数，实际上等于LogSoftmax+NLLLoss
     # LogSoftmax=log+softmax
     # NLLLoss:negative log likelihood loss
-    loss_func = torch.nn.CrossEntropyLoss()#损失函数
-    opt = torch.optim.Adam(model.parameters(), lr=1e-3)#优化器（梯度下降的具体算法Adam）
+    loss_func = torch.nn.CrossEntropyLoss()  # 损失函数
+    opt = torch.optim.Adam(model.parameters(), lr=1e-3)  # 优化器（梯度下降的具体算法Adam）
 
     loss = np.zeros(epochs)
     model.train()  # 会打开dropout、batchnorm等
-    for i in range(epochs):        
+    for i in range(epochs):
         loss_sum = 0
         time_start, time_pre = datetime.datetime.now(), datetime.datetime.now()
         for j, info in enumerate(data_loader_train):
             images, labels = info
             if cuda_ok:
                 images, labels = images.cuda(), labels.cuda()
-            y_pred = model(images)#1.先预测
-            loss_cur = loss_func(y_pred, labels)#2.计算损失
-            #暂不需看
+            y_pred = model(images)  # 1.先预测
+            loss_cur = loss_func(y_pred, labels)  # 2.计算损失
+            # 暂不需看
             loss_sum += loss_cur.item()
             if (j+1) % 10 == 0:
                 time_now = datetime.datetime.now()
                 print((j+1)*nbatch, "images input,", "avarage loss:", loss_sum/(j+1),
-                    ", duration:", (time_now-time_pre).total_seconds())
+                      ", duration:", (time_now-time_pre).total_seconds())
                 time_pre = time_now
-            #暂不需看
-            opt.zero_grad()#3.对之前的梯度清零
-            loss_cur.backward()#4.计算反向传播梯度
-            opt.step()#5.反向传播的梯度更新
-            #暂不需看
+            # 暂不需看
+            opt.zero_grad()  # 3.对之前的梯度清零
+            loss_cur.backward()  # 4.计算反向传播梯度
+            opt.step()  # 5.反向传播的梯度更新
+            # 暂不需看
             del images, labels, y_pred, loss_cur
-            torch.cuda.empty_cache() 
+            torch.cuda.empty_cache()
         loss[i] = loss_sum/math.ceil(data.GetLen()/nbatch)
-        print("[epochs ", i, "/", epochs, "] loss:", loss[i], "duration:",
-            (datetime.datetime.now()-time_start).total_seconds())       
-                
+        print("[epochs ", i+1, "/", epochs, "] loss:", loss[i], "duration:",
+              (datetime.datetime.now()-time_start).total_seconds())
+
     # 绘图
     plt.figure()
     plt.plot([x for x in range(0, epochs)], loss)
