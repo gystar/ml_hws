d6b050c435500a1d8c26511d9353ffe2f8246bde
gystar <gy163email@163.com>
2020-12-01 20:34:17 +0800

    添加了一个画彩色filter的方法

--- hw5/filter_visualization.py
+++ hw5/filter_visualization.py
@@ -1,96 +1,43 @@
+import matplotlib
 import matplotlib.pyplot as plt
 from PIL import Image
 from torchvision import transforms
 import numpy as np
-
-# TODO: Feel free to try out your own images here by changing img_path
-# to a file path to another image on your computer!
-img_path = '../hw3/data/training/0_0.jpg'
-
-# load color image 
-gray_img = np.array(Image.open(img_path).convert("L"))
-plt.imshow(gray_img, cmap='gray')
-plt.show()
-
-
-import numpy as np
-
-## TODO: Feel free to modify the numbers here, to try out another filter!
-filter_vals = np.array([[-1, -1, 1, 1], [-1, -1, 1, 1], [-1, -1, 1, 1], [-1, -1, 1, 1]])
-
-print('Filter shape: ', filter_vals.shape)
-
-
+import os,sys
+import importlib
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import math
 
+sys.path.append('../hw3') ##直接使用hw3中的model
+import image_classification
+import image_set
+importlib.reload(image_set)
+importlib.reload(image_classification)
+
+current_dir = os.path.dirname(__file__)
+MODEL_PATH =  os.path.join(current_dir, "../hw3/<class 'image_classification.GYHF_LetNet5'>.pkl")
+TRAIN_DIR = os.path.join(current_dir, "../hw3/data/training")
+OUTPUT_DIR = os.path.join(current_dir, "output")
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
     
-# define a neural network with a convolutional layer with four filters
-# AND a pooling layer of size (4, 4)
-class Net(nn.Module):
-    
-    def __init__(self, weight):
-        super(Net, self).__init__()
-        # initializes the weights of the convolutional layer to be the weights of the 4 defined filters
-        k_height, k_width = weight.shape[2:]
-        # assumes there are 4 grayscale filters
-        self.conv = nn.Conv2d(1, 4, kernel_size=(k_height, k_width), bias=False)
-        self.conv.weight = torch.nn.Parameter(weight)
-        # define a pooling layer
-        self.pool = nn.MaxPool2d(4, 4)
+#指定模型类别
+model_class = image_classification.GYHF_LetNet5
+data = image_set.LearningSet( TRAIN_DIR, model_class.input_size, False)
+#使用已经训练好的model
+model = torch.load(MODEL_PATH)
 
-    def forward(self, x):
-        # calculates the output of a convolutional layer
-        # pre- and post-activation
-        conv_x = self.conv(x)
-        activated_x = F.relu(conv_x)
-        
-        # applies pooling layer
-        pooled_x = self.pool(activated_x)
-        
-        # returns all layers
-        return conv_x, activated_x, pooled_x
-# Defining four different filters,
-# all of which are linear combinations of the `filter_vals` defined above
- 
-# define four filters
-filter_1 = filter_vals
-filter_2 = -filter_1
-filter_3 = filter_1.T
-filter_4 = -filter_3
-filters = np.array([filter_1, filter_2, filter_3, filter_4])
- 
-# For an example, print out the values of filter 1
-print('Filter 1: \n', filter_1)   
-# instantiate the model and set the weights
-weight = torch.from_numpy(filters).unsqueeze(1).type(torch.FloatTensor)
-model = Net(weight)
-
-# print out the layer in the network
-print(model)
-
-
-# helper function for visualizing the output of a given layer
-# default number of filters is 4
-def viz_layer(layer, n_filters= 4):
+def plot_layer(filters ):
+    #输入的layers为[layer_count, w, h]
     fig = plt.figure(figsize=(20, 20))
+    cols = 3
+    rows = math.ceil(filters.shape[0]/cols)
     
-    for i in range(n_filters):
-        ax = fig.add_subplot(1, n_filters, i+1, xticks=[], yticks=[])
+    for i in range(filters.shape[0]):
+        rgb_matrix = np.array(transforms.ToPILImage()(filters[i].detach()))
+        ax = fig.add_subplot(rows, cols, i+1, xticks=[], yticks=[])
         # grab layer outputs
-        ax.imshow(np.squeeze(layer[0,i].data.numpy()), cmap='gray')
-        ax.set_title('Output %s' % str(i+1)) 
+        ax.imshow(rgb_matrix)
+        ax.set_title('Filter %s' % str(i+1), fontsize=50,color='g')  
 
-    
-# convert the image into an input Tensor
-gray_img_tensor = torch.from_numpy(gray_img).unsqueeze(0).unsqueeze(1)
-
-# get all the layers 
-conv_layer, activated_layer, pooled_layer = model(gray_img_tensor.float())
-
-# visualize the output of the activated conv layer
-viz_layer(activated_layer)
-
-# visualize the output of the pooling layer
-viz_layer(pooled_layer)
+plot_layer(model.features[0].weight)
