bd2487e796f602c67329cdda385ae089360e7a4d
gystar <gy163email@163.com>
2021-04-06 20:52:39 +0800

    上传hw10的题目

--- hw9/main.py
+++ hw9/main.py
@@ -23,7 +23,7 @@ MODEL_PATH = f"./data/{str(encoder_class)}.pkl"
 if os.path.exists(MODEL_PATH):
     encoder = model_manager.load_model(encoder, MODEL_PATH, device)
 
-if True:
+if False:
     # train the model
     data_train = image_set.NoLabeledSet(True)
     encoder = model_manager.train_model(
@@ -63,21 +63,28 @@ data_val = image_set.ValidationSet()
 # 1.不使用encoder直接用KMeans
 pred = utils.direct_kpredict(data_val.images.cpu().view(data_val.images.shape[0], -1))
 rigth_num = len((pred == data_val.labels.squeeze().numpy()).nonzero()[0])
-print(f"the accuracy of clustering with direct kmeans:{100*rigth_num/data_val.__len__()}%")
+print(
+    f"the accuracy of clustering with direct kmeans:{100*rigth_num/data_val.__len__()}%"
+)
 
 # 2.不使用encoder，但是使用PCA+TSNE+KMeans
 pred = utils.predict(data_val.images.cpu().view(data_val.images.shape[0], -1))
 rigth_num = len((pred == data_val.labels.squeeze().numpy()).nonzero()[0])
-print(f"the accuracy of clustering with complex kmeans :{100*rigth_num/data_val.__len__()}%")
+print(
+    f"the accuracy of clustering with complex kmeans :{100*rigth_num/data_val.__len__()}%"
+)
 
-# 3.使用encoder直接用KMeans
+# 3.使用encoder使用单纯的KMeans
 codes = model_manager.encode(encoder, device, data_val).numpy()
 pred = utils.direct_kpredict(codes)
 rigth_num = len((pred == data_val.labels.squeeze().numpy()).nonzero()[0])
-print(f"the accuracy of clustering with autoencoder and kmeans:{100*rigth_num/len(codes)}%")
+print(
+    f"the accuracy of clustering with autoencoder and kmeans:{100*rigth_num/len(codes)}%"
+)
 
 # 4.使用encoder,同时使用PCA+TSNE+KMeans
-codes = model_manager.encode(encoder, device, data_val).numpy()
 pred = utils.predict(codes)
 rigth_num = len((pred == data_val.labels.squeeze().numpy()).nonzero()[0])
-print(f"tthe accuracy of clustering with autoencoder and complex kmeans:{100*rigth_num/len(codes)}%")
+print(
+    f"tthe accuracy of clustering with autoencoder and complex kmeans:{100*rigth_num/len(codes)}%"
+)
